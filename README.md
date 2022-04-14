2d6ef62882aac24e75cb6afb3f5f0d2932a05170
preiter <philipp.reiter@ds.mpg.de>
2022-04-14 17:20:45 +0200

    first commit

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # pencil_decomp
 
-## `mpi_pencil`: Library for mpi pencil distributions
+## `pencil_decomp`: Library for MPI pencil distributions
+
+## Explanation
+['Pencil decomposition in 3D'](https://github.com/preiter93/pencil_decomp/tree/master/pics/pencil2.pdf)
 
 Work in progress...
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
-//! # `mpi_pencil`: Library for mpi pencil distributions
+//! # `pencil_decomp`: Library for MPI pencil distributions
+//!
+//! # Explanation
+//! ['Pencil decomposition in 3D'](https://github.com/preiter93/pencil_decomp/tree/master/pics/pencil2.pdf)
 //!
 //! Work in progress...
 #![warn(clippy::pedantic)]
