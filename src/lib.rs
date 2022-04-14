9537f0af58f72742772e4e0bb204cd11feade537
preiter <philipp.reiter@ds.mpg.de>
2022-04-14 17:56:51 +0200

    first commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,8 @@
 //! # `pencil_decomp`: Library for MPI pencil distributions
 //!
 //! ## Documentation
-//! <div align="left">
-//! <img src="https://github.com/preiter93/pencil_decomp/blob/master/pics/pencil2.png?raw=true" width="500"></img>
-//! </div>
 //!
-//! ![Alt version](https://github.com/preiter93/pencil_decomp/blob/master/pics/pencil2.png)
+//! <img align="left" src="https://github.com/preiter93/pencil_decomp/blob/master/pics/pencil2.png?raw=true" width="200">
 //!
 //! ## Notes
 //! Work in progress...
